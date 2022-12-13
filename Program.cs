b23839e5404c08dfdc5628fd3a2b441a3a11b3c2
Daniel Aikins <117459632+Aikins0@users.noreply.github.com>
2022-12-13 16:15:28 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,122 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+public class cardHolder
+{
+    string cardNum;
+    int pin;
+    string firstName;
+    string lastName;
+    double balance;
+
+    public cardHolder(string cardNum, int pin, string firstName, string lastName, double balance)
+    {
+        this.cardNum = cardNum;
+        this.pin = pin;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.balance = balance;
+    }
+
+    public string getNum()
+    {
+        return cardNum;
+    }
+
+    public int getPin()
+    {
+        return pin;
+    }
+
+    public string getFirstName()
+    {
+        return firstName;
+    }
+
+    public string getLastName()
+    {
+        return lastName;
+    }
+
+    public double getBalance()
+    {
+        return balance;
+    }
+
+    public void setNum(string newCardNum)
+    { 
+        cardNum = newCardNum;
+    }
+
+    public void setPin(int newPin)
+    {
+        pin = newPin;
+    }
+
+    public void setFirstName(string newFirstName)
+    {
+        firstName = newFirstName;
+    }
+
+    public void setLastName(string newLastName)
+    {
+        lastName = newLastName;
+    }
+
+    public void setBalance(double newBalance)
+    {
+        balance = newBalance;
+    }
+
+    public static void Main(string[] args) 
+    { 
+        void printOptions()
+        {
+            Console.WriteLine("Please choose from one of the following options.....");
+            Console.WriteLine("1. Deposit");
+            Console.WriteLine("2. Withdraw");
+            Console.WriteLine("3. Show Balance");
+            Console.WriteLine("4. Exit");
+        }
+
+        void deposit(cardHolder currentUser)
+        {
+            Console.WriteLine("How much $$ would you like to deposit");
+            double deposit = Double.Parse(Console.ReadLine());
+            currentUser.setBalance(deposit);
+            Console.WriteLine("Thank you for your $$. Your new balance is : " + currentUser.getBalance);
+        }
+
+        void withdraw(cardHolder currentUser)
+        {
+            Console.WriteLine("How much $$ would you like to withdraw: ");
+            double withdraw = Double.Parse(Console.ReadLine());
+            //check if the user has enough balance
+            if(currentUser.getBalance() > withdraw)
+            {
+                Console.WriteLine("Insufficient balance :(");
+            }
+            else
+            {
+                currentUser.setBalance(currentUser.getBalance() - withdraw);
+                Console.WriteLine("You're good to go! Thank you :)");
+            }
+        }
+
+        void balance(cardHolder currentUser)
+        {
+            Console.WriteLine("Current balance: " + currentUser.getBalance());
+        }
+            
+        List<cardHolder> cardHolders = new List<cardHolder>();
+        cardHolder.Add(new cardHolder("4532772818527395", 1234, "John", "Griffith", 150.31));
+
+
+        // prompt user
+        Console.WriteLine("Welcome to MyATM");
+        Console.WriteLine("Please insert your debit card: ");
+        string debitCardNum = "";
+        cardHolder currentUser;
+
+    }
+}
